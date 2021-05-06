0cf2c7fc1246643f181068aee79c8b634513cf55
chokrihamza <chokrihamza1994@gmail.com>
2021-05-06 16:22:20 +0200

    work on blog

---
+++ includes/db.php
@@ -0,0 +1,21 @@
+<?php
+   
+$db['db_host']='localhost';
+$db['db_user']='root';
+$db['db_pass']='';
+$db['db_name']='cms';
+
+foreach($db as $key=>$value){
+  define(strtoupper($key),$value);
+ 
+}
+
+ 
+$connection=mysqli_connect('localhost','root','','cms');
+/*if($connection){
+ echo 'we are connected';
+}*/
+
+
+   
+?>
