d73eb6e8a2dd8dfcaf1ba2d4351cc9168b64dcb3
chokrihamza <chokrihamza1994@gmail.com>
2021-05-08 14:11:16 +0200

    work on categories

--- admin/categories.php
+++ admin/categories.php
@@ -1,9 +1,9 @@
-<?php include "includes/header.php" ?>
+<?php include "includes/admin_header.php" ?>
 
 <div id="wrapper">
 
   <!-- Navigation -->
-  <?php include "includes/navigation.php"   ?>
+  <?php include "includes/admin_navigation.php"   ?>
   <div id="page-wrapper">
 
     <div class="container-fluid">
@@ -16,15 +16,88 @@
             <small>Author</small>
           </h1>
           <div class="col-xs-6">
-            <form action="">
+            <?php  
+           if (isset($_POST['submit'])) {
+               $cat_title=$_POST['cat_title'];
+               
+               if ($cat_title==''||empty($cat_title)) {
+                   echo "This field should not be empty";
+               } else {
+                   $query="INSERT INTO categories(cat_title) VALUE('{$cat_title}') ";
+                   
+                   $create_catregory_query = mysqli_query($connection, $query);
+                  
+                   if (!$create_catregory_query) {
+                       die('Query Failed' . mysqli_error($connection));
+                   }
+               }
+           }
+          
+          
+          ?>
+
+
+
+            <form action="" method="post">
               <div class="form-group">
                 <label for="cat_title">Add Category</label>
                 <input type="text" class="form-control" name="cat_title" />
               </div>
               <div class="form-group">
-                <input class="btn btn-primary" type="sumbit" name="submit" value="Add Category" />
+                <input class="btn btn-primary" type="submit" name="submit" value="Add Category" />
               </div>
             </form>
+            <?php
+               if(isset($_GET['edit'])){
+                 $cat_id=$_GET['edit'];
+                 include "includes/update_categories.php";
+               }
+            ?>
+          </div>
+          <div class="col-xs-6">
+            <?php
+              
+               ?>
+            <table class='table table-bordered table-hover'>
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Catergory Title</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php // Find all categories query
+                 $query = "SELECT * FROM categories ";
+                 $select_categories = mysqli_query($connection,$query);
+                  while    ($row=mysqli_fetch_assoc($select_categories))    {
+                  $cat_id= $row['cat_id'];
+                  $cat_title= $row['cat_title'];
+                  echo "<tr>";
+                  echo "<td>{$cat_id}</td>";
+                  echo "<td>{$cat_title}</td>";
+                  echo "<td><a href='categories.php?delete={$cat_id}'>DELETE</a></td>";
+                  echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
+                  echo "</tr>" ;
+              
+                         }
+                ?>
+
+                <?php // Delete query
+                   if(isset($_GET['delete'])){
+                     $the_cat_id=$_GET['delete'];
+                     
+                     $query="DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
+                     $delete_query=mysqli_query($connection,$query);
+                     header("Location:categories.php");
+
+
+                   }
+                ?>
+
+              </tbody>
+
+            </table>
+
           </div>
         </div>
       </div>
@@ -36,4 +109,4 @@
   </div>
   <!-- /#page-wrapper -->
 </div>
-<?php include "includes/footer.php"   ?>
+<?php include "includes/admin_footer.php"?>
