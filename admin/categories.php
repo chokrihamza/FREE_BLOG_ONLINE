e981ba2a94c1f733f1945cc8b99237dac4b6255c
chokrihamza <chokrihamza1994@gmail.com>
2021-05-08 14:43:03 +0200

    second refactoring

--- admin/categories.php
+++ admin/categories.php
@@ -26,11 +26,11 @@
                 <input class="btn btn-primary" type="submit" name="submit" value="Add Category" />
               </div>
             </form>
-            <?php
-                                                if (isset($_GET['edit'])) {
-                                                  $cat_id = $_GET['edit'];
-                                                  include "includes/update_categories.php";
-                                                }
+            <?php //update and include query
+            if (isset($_GET['edit'])) {
+              $cat_id = $_GET['edit'];
+              include "includes/update_categories.php";
+            }
             ?>
           </div>
           <div class="col-xs-6">
@@ -45,30 +45,10 @@
                 </tr>
               </thead>
               <tbody>
-                <?php // Find all categories query
-                                                                $query = "SELECT * FROM categories ";
-                                                $select_categories = mysqli_query($connection, $query);
-                                                while ($row = mysqli_fetch_assoc($select_categories)) {
-                                                  $cat_id = $row['cat_id'];
-                                                  $cat_title = $row['cat_title'];
-                  echo "<tr>";
-                  echo "<td>{$cat_id}</td>";
-                  echo "<td>{$cat_title}</td>";
-                  echo "<td><a href='categories.php?delete={$cat_id}'>DELETE</a></td>";
-                  echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
-                  echo "</tr>";
-                }
-                ?>
-
-                <?php // Delete query
-                                                                if (isset($_GET['delete'])) {
-                                                                  $the_cat_id = $_GET['delete'];
-
-                  $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
-                  $delete_query = mysqli_query($connection, $query);
-                  header("Location:categories.php");
-                }
-                ?>
+                <!-- Find all categories query-->
+                <?php findAllCategories();  ?>
+                <!-- Delete query -->
+                <?php  deleteCategories()?>
 
               </tbody>
 
--- admin/functions.php
+++ admin/functions.php
@@ -19,3 +19,32 @@ function insert_catergories()
     }
   }
 }
+
+function findAllCategories()
+{
+  global $connection;
+  $query = "SELECT * FROM categories ";
+  $select_categories = mysqli_query($connection, $query);
+  while ($row = mysqli_fetch_assoc($select_categories)) {
+    $cat_id = $row['cat_id'];
+    $cat_title = $row['cat_title'];
+    echo "<tr>";
+    echo "<td>{$cat_id}</td>";
+    echo "<td>{$cat_title}</td>";
+    echo "<td><a href='categories.php?delete={$cat_id}'>DELETE</a></td>";
+    echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
+    echo "</tr>";
+  }
+}
+function deleteCategories()
+{
+  global $connection;
+  if (isset($_GET['delete'])) {
+    $the_cat_id = $_GET['delete'];
+    $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
+    $delete_query = mysqli_query($connection, $query);
+    header("Location:categories.php");
+  }
+}
+
+?>
