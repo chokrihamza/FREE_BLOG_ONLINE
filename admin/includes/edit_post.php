7afd1185685cfeb9e0f80a19906ac241cdc3b366
chokrihamza <chokrihamza1994@gmail.com>
2021-05-09 13:39:26 +0200

    finish edit post

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -20,14 +20,28 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 if (isset($_POST['update_post'])) {
 
   $post_author = $_POST['author'];
-  $$post_title = $_POST['title'];
-  $post_category_id = $_POST['post_category_id'];
+  $post_title = $_POST['title'];
+  $post_category_id = $_POST['post_category'];
   $post_status = $_POST['post_status'];
   $post_image = $_FILES['image']['name'];
   $post_image_temp = $_FILES['image']['tmp_name'];
   $post_content = $_POST['post_content'];
   $post_tags = $_POST['post_tags'];
   move_uploaded_file($post_image_temp, "../images/$post_image");
+  $query = "UPDATE posts SET ";
+  $query .= "post_title='{$post_title}', ";
+  $query .= "post_category_id='{$post_category_id}', ";
+  $query .= "post_date= now(), ";
+  $query .= "post_author='{$post_author}', ";
+  $query .= "post_status='{$post_status}', ";
+  $query .= "post_tags='{$post_tags}', ";
+  $query .= "post_content='{$post_content}', ";
+  $query .= "post_image='{$post_image}' ";
+  $query .= "WHERE post_id={$the_post_id} ";
+
+  $update_post=mysqli_query($connection,$query);
+  confirmQuery($update_post);
+
 }
 
 ?>
@@ -37,7 +51,7 @@ if (isset($_POST['update_post'])) {
     <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="title" />
   </div>
   <div class="form-group">
-    <select name="" id="">
+    <select name="post_category" id="">
       <?php
       $query = "SELECT * FROM categories";
       $select_categories = mysqli_query($connection, $query);
@@ -45,7 +59,7 @@ if (isset($_POST['update_post'])) {
       while ($row = mysqli_fetch_assoc($select_categories)) {
         $cat_id = $row['cat_id'];
         $cat_title = $row['cat_title'];
-        echo "<option value=''>$cat_title</option>";
+        echo "<option value='$cat_id'>$cat_title</option>";
       }
       ?>
 
@@ -61,6 +75,7 @@ if (isset($_POST['update_post'])) {
   </div>
   <div class="form-group">
     <img width="100" src="../images/<?php echo $post_image; ?>" alt="" />
+    <input type="file" class="form-control" name="image" />
   </div>
   <div class="form-group">
     <label for="post_tags">Post Tags</label>
@@ -73,7 +88,7 @@ if (isset($_POST['update_post'])) {
     </textarea>
   </div>
   <div>
-    <input class='btn btn-primary' type="submit" name="update_post" value="Publish Post" />
+    <input class='btn btn-primary' type="submit" name="update_post" value="Edit Post" />
   </div>
 
 
