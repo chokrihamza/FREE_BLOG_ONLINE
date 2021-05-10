4d649cf98f48dd8648999950be6cb9b2d41d2b9c
chokrihamza <chokrihamza1994@gmail.com>
2021-05-10 12:31:45 +0200

    adding comments panel

--- admin/comments.php
+++ admin/comments.php
@@ -16,26 +16,25 @@
             <small>Author</small>
           </h1>
           <?php
-             if(isset($_GET['source'])){
-               $source=$_GET['source'];
-
-             }else{
-              $source='';
-             }
-             switch ($source) {
-                case 'add_post':
-                 include "includes/add_post.php";
-                 break;
+          if (isset($_GET['source'])) {
+            $source = $_GET['source'];
+          } else {
+            $source = '';
+          }
+          switch ($source) {
+            case 'add_post':
+              include "includes/add_post.php";
+              break;
             case 'edit_post':
               include "includes/edit_post.php";
               break;
             default:
-                 include "includes/view_all_posts.php";
-                 break;
-             }
+              include "includes/view_all_comments.php";
+              break;
+          }
 
 
-           
+
           ?>
         </div>
       </div>
--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -64,7 +64,7 @@
         </ul>
       </li>
       <li class="active">
-        <a href="blank-page.html"><i class="fa fa-fw fa-file"></i>Comments</a>
+        <a href="comments.php"><i class="fa fa-fw fa-file"></i>Comments</a>
       </li>
       <li>
         <a href="javascript:;" data-toggle="collapse" data-target="#users"><i class="fa fa-fw fa-arrows-v"></i>
