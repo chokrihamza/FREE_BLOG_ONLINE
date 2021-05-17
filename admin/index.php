e2d9097ffbba86c0fec22c6353d27eae3929f7eb
chokrihamza <chokrihamza1994@gmail.com>
2021-05-17 16:28:52 +0200

    work on logout

--- admin/index.php
+++ admin/index.php
@@ -13,7 +13,7 @@
         <div class="col-lg-12">
           <h1 class="page-header">
             Welcome to the Admin panel
-            <small>Author</small>
+            <small><?php echo $_SESSION['username']; ?></small>
           </h1>
 
         </div>
--- includes/login.php
+++ includes/login.php
@@ -1,4 +1,5 @@
 <?php include "db.php"; ?>
+<?php session_start(); ?>
 
 <?php
 if (isset($_POST['login'])) {
@@ -14,7 +15,24 @@ if (isset($_POST['login'])) {
   }
 
   while ($row = mysqli_fetch_array($select_user_query)) {
-    echo $db_id = $row['user_id'];
+    $db_user_id = $row['user_id'];
+    $db_username = $row['username'];
+    $db_user_password = $row['user_password'];
+    $db_user_firstname = $row['user_firstname'];
+    $db_user_lastname = $row['user_lastname'];
+    $db_user_role = $row['user_role'];
+  }
+ 
+if ($username === $db_username && $password === $db_user_password) {
+    
+    $_SESSION['username']  =  $db_username;
+    $_SESSION['firstname']  =  $db_user_firstname;
+    $_SESSION['lastname']  =  $db_user_lastname;
+    $_SESSION['user_role']  =  $db_user_role;
+    header("Location: ../admin");
+  
+  } else {
+    header("Location: ../index.php");
   }
 }
 ?>
