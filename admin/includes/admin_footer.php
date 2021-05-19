1721b353333b25efae2e7762498dda32e7d1be8e
chokrihamza <chokrihamza1994@gmail.com>
2021-05-19 12:25:32 +0200

    extrat feature 3

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -2,7 +2,7 @@
 
 <!-- jQuery -->
 <script src="js/jquery.js"></script>
-
+<script src="js/scripts.js"></script>
 <!-- Bootstrap Core JavaScript -->
 <script src="js/bootstrap.min.js"></script>
 
--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -2,3 +2,20 @@
 tinymce.init({
   selector: '#mytextarea'
 });
+
+$(document).ready(function () {
+  $('#selectAllPosts').click(function (event) {
+    if (this.checked) {
+
+      $('.checkBoxes').each(function () {
+        this.checked = true;
+      });
+    } else {
+
+
+      $('.ckeckBoxes').each(function () {
+        this.checked = false;
+      });
+    }
+  });
+});
