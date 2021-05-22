746cc42e51e6596182db5e57e317feb1e39371dd
chokrihamza <chokrihamza1994@gmail.com>
2021-05-22 11:24:38 +0200

    add advance count users online

--- admin/functions.php
+++ admin/functions.php
@@ -9,7 +9,7 @@ function users_online()
       include('../includes/db.php');
       $session = session_id();
       $time = time();
-      $time_out_in_seconds = 60;
+      $time_out_in_seconds = 3;
       $time_out = $time - $time_out_in_seconds;
       $query = "SELECT * FROM users_online WHERE session='$session' ";
       $send_query = mysqli_query($connection, $query);
@@ -19,7 +19,7 @@ function users_online()
       } else {
         mysqli_query($connection, "UPDATE users_online SET time='$time' WHERE session='$session'");
       }
-
+      $users_online_query = mysqli_query($connection, "DELETE  FROM users_online WHERE time<'$time_out'");
       $users_online_query = mysqli_query($connection, "SELECT * FROM users_online WHERE time>'$time_out'");
       $count_user = mysqli_num_rows($users_online_query);
       echo $count_user;
